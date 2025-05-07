8bf2a12770b539587b9360aaee097afe87b76c2b
simonetassi <72198236+simonetassi@users.noreply.github.com>
2025-05-07 15:33:53 +0200

    update nvim neotree keybindings and fix lsp bug

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -8,6 +8,18 @@ return {
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     config = function()
-		vim.keymap.set('n', '<C-l>', ':Neotree filesystem toggle right<CR>', {})
+      require("neo-tree").setup({
+        event_handlers = {
+
+          {
+            event = "file_open_requested",
+            handler = function()
+             require("neo-tree.command").execute({ action = "close" })
+            end
+          },
+
+        }
+      })
+		vim.keymap.set('n', '<C-l>', ':Neotree filesystem reveal right<CR>', {})
     end
 }
